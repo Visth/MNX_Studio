32b18d66212dd17eeff10009518e3b8cbfc8342c
Filip <filber43@gmail.com>
2023-08-18 18:00:57 +0200

    added actual date in footer.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 const nav = document.querySelector('.navbar-collapse')
+const actualDate = document.querySelector('.actual-date');
 
 document.addEventListener('click', () => {
     if (nav.classList.contains('show')) {
         nav.classList.remove('show')
     }
 })
+
+const handleCurrentYear = () => {
+  const year = (new Date).getFullYear();
+  actualDate.innerText = year;
+}
+handleCurrentYear();
